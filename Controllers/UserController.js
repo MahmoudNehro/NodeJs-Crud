de9ae3ad20636a5ddf874954dedc4c49dd56b337
MahmoudNehro <mahmoudnehro9000@gmail.com>
2023-01-16 01:18:29 +0200

    User Controller

---
+++ Controllers/UserController.js
@@ -0,0 +1,65 @@
+const User = require("../Models/User");
+class UserController {
+    async getAllUsers(req, res) {
+        const users = await User.findAndCountAll();
+        console.log(users);
+        res.send({
+            data: users.rows,
+            total: users.count
+        });
+    }
+    async getUser(req, res) {
+        const id = req.params.id;
+        await User.findByPk(id).then((user) => {
+            if (user) {
+                res.send({
+                    data: user
+                });
+            } else {
+
+
+                res.sendStatus(404);
+            }
+        });
+    }
+    async createUser(req, res) {
+        console.log(req.body);
+        const user = {
+            name: req.body.name,
+            email: req.body.email
+        };
+        await User.create(user).then(() => {
+            res.sendStatus(201);
+        });
+    }
+    async updateUser(req, res) {
+        const id = req.params.id;
+        await User.findByPk(id).then((user) => {
+            console.log(user, req.body);
+            if (user != null) {
+                user.update({
+                    name: req.body.name,
+                    email: req.body.email
+                }).then(() => {
+                    res.sendStatus(204);
+                });
+            } else {
+                res.sendStatus(404);
+            }
+        });
+    }
+    async deleteUser(req, res) {
+        const id = req.params.id;
+        await User.findByPk(id).then((user) => {
+            if (user != null) {
+                user.destroy();
+                res.sendStatus(200);
+            } else {
+                res.sendStatus(404);
+            }
+        });
+    }
+}
+
+module.exports = new UserController();
+
